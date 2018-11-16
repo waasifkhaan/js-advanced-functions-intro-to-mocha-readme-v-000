2dc79c52fee087e4f94ca602352eeedf71c61e3d
Wasif Khan <waasif.khaan@gmail.com>
2018-11-16 02:56:38 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,24 @@
+function theTruth() {
+  return true
+}
+function sayHey() {
+  return "Hey!"
+}
+function sayHeyFriend(name){
+  return `Hey, ${name}!`;
+}
+
+function favoriteIceCream(flavor){
+  return `I love ${flavor}`
+
+}
+
+function shouting(name){
+  return name.toUpperCase()
+
+}
+
+function roundDown(number){
+  return Math.floor(number)
+
+}
